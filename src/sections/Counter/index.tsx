d9a89a5167241f23a4ab8977b0fbb0fd26d26299
Qiuran Hu <qiuranh@gmail.com>
2020-11-30 23:04:20 +0800

    Counter

---
+++ src/sections/Counter/index.tsx
@@ -0,0 +1,85 @@
+import React, { FormEvent, useEffect, useState } from "react";
+import "@material/react-button/dist/button.css";
+import "@material/react-typography/dist/typography.css";
+import "./style.css";
+import Button from "@material/react-button";
+import { Headline1 } from "@material/react-typography";
+import "@material/react-material-icon/dist/material-icon.css";
+import MaterialIcon from "@material/react-material-icon";
+import "@material/react-text-field/dist/text-field.css";
+import TextField, { Input } from "@material/react-text-field";
+
+interface CounterState {
+  number: number;
+  changeBy: number;
+}
+
+const initialState = {
+  number: 0,
+  changeBy: 10,
+};
+
+export const Counter = () => {
+  const [state, setState] = useState<CounterState>(initialState);
+  useEffect(() => {
+    const lastState = localStorage.getItem("counter");
+    if (lastState) {
+      setState(JSON.parse(lastState));
+    }
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("counter", JSON.stringify(state));
+  }, [state]);
+  const changeNumber = (newNumber: number) => {
+    if (newNumber < 0) {
+      return;
+    }
+    setState({
+      ...state,
+      number: newNumber,
+    });
+  };
+  const setNewChangeBy = (newChangeBy: string) => {
+    const changeBy = Number(newChangeBy);
+    console.log(changeBy);
+    if (Number.isNaN(changeBy)) {
+      return;
+    }
+    setState({
+      ...state,
+      changeBy: Number(newChangeBy),
+    });
+  };
+  return (
+    <>
+      <Headline1 className="counter__number">{state.number}</Headline1>
+      <div className="counter__button-container">
+        <div className="counter__change-by-input">
+          <TextField outlined={true} label="Change By">
+            <Input
+              value={state.changeBy}
+              onChange={(e: FormEvent<HTMLInputElement>): void =>
+                setNewChangeBy(e.currentTarget.value)
+              }
+            />
+          </TextField>
+        </div>
+        <span className="counter__buttons">
+          <Button
+            onClick={() => changeNumber(state.number - state.changeBy)}
+            className="counter__button-minus"
+          >
+            <MaterialIcon icon="indeterminate_check_box" />
+          </Button>
+
+          <Button
+            onClick={() => changeNumber(state.number + state.changeBy)}
+            className="counter__button-add"
+          >
+            <MaterialIcon icon="add_box" />
+          </Button>
+        </span>
+      </div>
+    </>
+  );
+};

7be8a4a1ac9957de679749c7fdf6fa74c7755027
Qiuran Hu <qiuranh@gmail.com>
2020-12-02 22:34:29 +0800

    Add card UI

--- src/sections/Counter/index.tsx
+++ src/sections/Counter/index.tsx
@@ -8,15 +8,17 @@ import "@material/react-material-icon/dist/material-icon.css";
 import MaterialIcon from "@material/react-material-icon";
 import "@material/react-text-field/dist/text-field.css";
 import TextField, { Input } from "@material/react-text-field";
+import Card from "@material/react-card";
+import "@material/react-card/dist/card.css";
 
 interface CounterState {
   number: number;
-  changeBy: number;
+  changeBy: string;
 }
 
 const initialState = {
   number: 0,
-  changeBy: 10,
+  changeBy: "10",
 };
 
 export const Counter = () => {
@@ -40,54 +42,68 @@ export const Counter = () => {
     });
   };
   const setNewChangeBy = (newChangeBy: string) => {
-    const changeBy = Number(newChangeBy);
-    if (Number.isNaN(changeBy)) {
-      return;
-    }
     setState({
       ...state,
-      changeBy: Number(newChangeBy),
+      changeBy: newChangeBy,
     });
   };
   const reset = () => {
     setState({
-      ...state,
+      changeBy: "10",
       number: 0,
     });
   };
+  const onChangeCounter = (type: "+" | "-") => {
+    const changeBy = Number(state.changeBy);
+    if (Number.isNaN(changeBy)) {
+      return;
+    }
+    let newNumber: number;
+    switch (type) {
+      case "+":
+        newNumber = state.number + changeBy;
+        break;
+      case "-":
+        newNumber = state.number - changeBy;
+        break;
+    }
+    changeNumber(newNumber);
+  };
   return (
-    <>
-      <Headline1 className="counter__number">{state.number}</Headline1>
-      <div className="counter__button-container">
-        <Button className="counter__button-reset" onClick={reset}>
-          Reset
-        </Button>
-        <div className="counter__change-by-input">
-          <TextField outlined={true} label="Change By">
-            <Input
-              value={state.changeBy}
-              onChange={(e: FormEvent<HTMLInputElement>): void =>
-                setNewChangeBy(e.currentTarget.value)
-              }
-            />
-          </TextField>
-        </div>
-        <span className="counter__buttons">
-          <Button
-            onClick={() => changeNumber(state.number - state.changeBy)}
-            className="counter__button-minus"
-          >
-            <MaterialIcon icon="indeterminate_check_box" />
+    <div className="counter__container">
+      <Card className="counter__wrapper" outlined>
+        <Headline1 className="counter__number">{state.number}</Headline1>
+        <div className="counter__button-container">
+          <Button className="counter__button-reset" onClick={reset}>
+            Reset
           </Button>
+          <div className="counter__change-by-input">
+            <TextField outlined={true} label="Change By">
+              <Input
+                value={state.changeBy}
+                onChange={(e: FormEvent<HTMLInputElement>): void =>
+                  setNewChangeBy(e.currentTarget.value)
+                }
+              />
+            </TextField>
+          </div>
+          <span className="counter__buttons">
+            <Button
+              onClick={() => onChangeCounter("-")}
+              className="counter__button-minus"
+            >
+              <MaterialIcon icon="indeterminate_check_box" />
+            </Button>
 
-          <Button
-            onClick={() => changeNumber(state.number + state.changeBy)}
-            className="counter__button-add"
-          >
-            <MaterialIcon icon="add_box" />
-          </Button>
-        </span>
-      </div>
-    </>
+            <Button
+              onClick={() => onChangeCounter("+")}
+              className="counter__button-add"
+            >
+              <MaterialIcon icon="add_box" />
+            </Button>
+          </span>
+        </div>
+      </Card>
+    </div>
   );
 };
